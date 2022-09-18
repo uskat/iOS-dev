2dcd40fdadbdad7a70fb2ce5a818f5e0c84e3546
uskat <uskat@rambler.ru>
2022-09-18 22:44:09 +0200

    finished HW-1 from iOS-dev

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,34 @@
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    var post: FeedViewController.Post?
+    
+    
+//MARK: - INITs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        showBarButton()
+        if let post = post {
+            title = post.title
+        }
+    }
+ 
+    override func viewWillLayoutSubviews() {
+        checkOrientation()
+    }
+
+    
+//MARK: - METHODs
+    private func showBarButton() {
+        let button = UIBarButtonItem(title: "Info...", style: .plain, target: self, action: #selector(tapAction))
+        navigationItem.rightBarButtonItem = button
+    }
+    
+    @objc private func tapAction() {
+        let forthVC = InfoViewController()
+        present(forthVC, animated: true)
+    }
+}
